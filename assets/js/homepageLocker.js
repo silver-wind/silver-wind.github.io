cf493ece1d066687d4302165bbed5810519bfd51
user <sliver-inbox@mail.ru>
2025-01-05 03:31:52 -0500

    update commit

--- assets/js/homepageLocker.js
+++ assets/js/homepageLocker.js
@@ -3,9 +3,14 @@ let clickCount = 0
 const htmlElem = document.getElementsByTagName('html')[0] //universal
 htmlElem.addEventListener('click', ()=>{
     clickCount++;
-    if (clickCount === 10) {
+    // if (clickCount === 10) {
+    if (window.location.hash === '#showProjects') {
+        // console.log('show')
+        // console.log(window.location.hash)
         document.cookie = "projects=visible"
 
         if(document.cookie === 'projects=visible') runLockControls()
+    } else {
+        document.cookie = "projects=hidden"
     }
 })
